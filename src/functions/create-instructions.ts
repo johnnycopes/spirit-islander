0fb0d141df3bd97dae9de2faf546a3b82948b479
John Coppola <jcoppola@insiten.com>
2021-03-11 07:21:51 -0500

    fixes broken import statements

--- src/functions/create-instructions.ts
+++ src/functions/create-instructions.ts
@@ -1,6 +1,6 @@
 import type { IInstructions } from "@models/instructions.interface";
 import type { ISelection } from "@models/selection.interface";
-import { selectRandom } from "@functions/utility/select-randomt-random";
+import { selectRandom } from "@functions/utility/select-random";
 import { getValidCombos } from "@functions/get-valid-combos";
 
 export function createInstructions(selection: ISelection): IInstructions {
--- src/functions/get-difficulty.ts
+++ src/functions/get-difficulty.ts
@@ -1,6 +1,6 @@
 import type { Difficulty } from "@models/game/difficulty";
 import type { IGameConfig } from "@models/game/game-config";
-import { getDynamicValue } from "@functions/utility/get-dynamic-valueic-value";
+import { getDynamicValue } from "@functions/utility/get-dynamic-value";
 
 export function getDifficulty(
 	difficulty: Difficulty | ((config: IGameConfig) => Difficulty),
