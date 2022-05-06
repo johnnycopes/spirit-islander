035011ae2ed52338a8b90cd470f3580c01290509
John Coppola <jdcoppola@gmail.com>
2022-05-06 19:29:40 -0400

    improves implementation of cleanArray and writes .spec file

--- src/functions/utility/clean-array.ts
+++ src/functions/utility/clean-array.ts
@@ -1,3 +1,9 @@
-export function cleanArray<T>(arr: T[]): T[] {
-	return [...new Set(arr)];
+export function cleanArray<T>(...arrays: T[][]): T[] {
+  const elements = [];
+
+  for (const arr of arrays) {
+    elements.push(...arr);
+  }
+
+  return Array.from(new Set(elements));
 }
