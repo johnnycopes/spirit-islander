45c9f8c72affae9947e248cf303059b0c3290010
John Coppola <jdcoppola@gmail.com>
2022-05-08 09:22:29 -0400

    improves select-random.spec

--- src/functions/utility/select-random.spec.ts
+++ src/functions/utility/select-random.spec.ts
@@ -6,10 +6,15 @@ describe("selectRandom", () => {
 		expect(() => selectRandom([1, 2, 3], 4)).toThrow();
 	});
 
-	it("returns the requested quantity of unique options", () => {
-		const options = [1, 2, 3];
+	it("returns the requested quantity of options", () => {
+		const options = [1, 2, 3, 4, 5];
 		const selected = selectRandom(options, 2);
 		expect(selected).toHaveLength(2);
+	});
+
+	it("returns unique options", () => {
+		const options = [1, 2];
+		const selected = selectRandom(options, 2);
 		expect(selected[0]).not.toBe(selected[1]);
 		expect(options.includes(selected[0])).toBe(true);
 		expect(options.includes(selected[1])).toBe(true);
