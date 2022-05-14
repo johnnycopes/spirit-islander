4830041d68befbc592bdec437c990bdc00710b68
John Coppola <jdcoppola@gmail.com>
2022-05-14 08:45:45 -0400

    begins writing tests for update-model

---
+++ src/functions/update-model.spec.ts
@@ -0,0 +1,144 @@
+import { createSpiritsModel } from "./create-model";
+import { updateModel } from "./update-model";
+
+describe("updateModel", () => {
+	it("adds items from individual expansion to existing model", () => {
+		expect(updateModel(
+			createSpiritsModel,
+			[],
+			["Branch & Claw"],
+			"Branch & Claw")
+		).toStrictEqual([
+			"Keeper of the Forbidden Wilds",
+			"Sharp Fangs Behind the Leaves",
+		]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			["Thunderspeaker"],
+			["Branch & Claw"],
+			"Branch & Claw")
+		).toStrictEqual([
+			"Thunderspeaker",
+			"Keeper of the Forbidden Wilds",
+			"Sharp Fangs Behind the Leaves",
+		]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			["Heart of the Wildfire", "Thunderspeaker"],
+			["Branch & Claw", "Promo Pack 1"],
+			"Branch & Claw")
+		).toStrictEqual([
+			"Heart of the Wildfire",
+			"Thunderspeaker",
+			"Keeper of the Forbidden Wilds",
+			"Sharp Fangs Behind the Leaves",
+		]);
+	});
+
+	it("adds items from all expansions to existing model", () => {
+		expect(updateModel(
+			createSpiritsModel,
+			[],
+			["Branch & Claw", "Jagged Earth", "Promo Pack 1", "Promo Pack 2"],
+			"Expansions")
+		).toStrictEqual([
+			"Downpour Drenches the World",
+			"Finder of Paths Unseen",
+			"Fractured Days Split the Sky",
+			"Grinning Trickster Stirs Up Trouble",
+			"Heart of the Wildfire",
+			"Keeper of the Forbidden Wilds",
+			"Lure of the Deep Wilderness",
+			"Many Minds Move as One",
+			"Serpent Slumbering Beneath the Island",
+			"Sharp Fangs Behind the Leaves",
+			"Shifting Memory of Ages",
+			"Shroud of Silent Mist",
+			"Starlight Seeks Its Form",
+			"Stone's Unyielding Defiance",
+			"Vengeance as a Burning Plague",
+			"Volcano Looming High",
+		]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			["Ocean's Hungry Grasp"],
+			["Branch & Claw", "Jagged Earth", "Promo Pack 1", "Promo Pack 2"],
+			"Expansions")
+		).toStrictEqual([
+			"Ocean's Hungry Grasp",
+			"Downpour Drenches the World",
+			"Finder of Paths Unseen",
+			"Fractured Days Split the Sky",
+			"Grinning Trickster Stirs Up Trouble",
+			"Heart of the Wildfire",
+			"Keeper of the Forbidden Wilds",
+			"Lure of the Deep Wilderness",
+			"Many Minds Move as One",
+			"Serpent Slumbering Beneath the Island",
+			"Sharp Fangs Behind the Leaves",
+			"Shifting Memory of Ages",
+			"Shroud of Silent Mist",
+			"Starlight Seeks Its Form",
+			"Stone's Unyielding Defiance",
+			"Vengeance as a Burning Plague",
+			"Volcano Looming High",
+		]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			["Keeper of the Forbidden Wilds", "Ocean's Hungry Grasp"],
+			["Branch & Claw", "Jagged Earth", "Promo Pack 1", "Promo Pack 2"],
+			"Expansions")
+		).toStrictEqual([
+			"Keeper of the Forbidden Wilds",
+			"Ocean's Hungry Grasp",
+			"Downpour Drenches the World",
+			"Finder of Paths Unseen",
+			"Fractured Days Split the Sky",
+			"Grinning Trickster Stirs Up Trouble",
+			"Heart of the Wildfire",
+			"Keeper of the Forbidden Wilds", // TODO: fix this bug where an item can be added twice if it already exists in the model and "Expansions" is selected
+			"Lure of the Deep Wilderness",
+			"Many Minds Move as One",
+			"Serpent Slumbering Beneath the Island",
+			"Sharp Fangs Behind the Leaves",
+			"Shifting Memory of Ages",
+			"Shroud of Silent Mist",
+			"Starlight Seeks Its Form",
+			"Stone's Unyielding Defiance",
+			"Vengeance as a Burning Plague",
+			"Volcano Looming High",
+		]);
+	});
+
+	it("removes expansion items from existing model", () => {
+		expect(updateModel(
+			createSpiritsModel,
+			["Keeper of the Forbidden Wilds", "Sharp Fangs Behind the Leaves"],
+			[],
+			"Branch & Claw",
+		)).toStrictEqual([]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			["Keeper of the Forbidden Wilds", "Sharp Fangs Behind the Leaves", "Thunderspeaker"],
+			[],
+			"Branch & Claw",
+		)).toStrictEqual(["Thunderspeaker"]);
+
+		expect(updateModel(
+			createSpiritsModel,
+			[
+				"Heart of the Wildfire",
+				"Keeper of the Forbidden Wilds",
+				"Sharp Fangs Behind the Leaves",
+				"Thunderspeaker",
+			],
+			["Promo Pack 1"],
+			"Branch & Claw",
+		)).toStrictEqual(["Heart of the Wildfire", "Thunderspeaker"]);
+	});
+});
