b66c027b75c3acbccc13c19a5f13ab8c268c7541
John Coppola <jdcoppola@gmail.com>
2022-05-16 08:14:10 -0400

    writes tests for getValidCombos

---
+++ src/functions/get-valid-combos.spec.ts
@@ -0,0 +1,79 @@
+import { getValidCombos } from "./get-valid-combos";
+import type { IConfig } from "@models/config.interface";
+import type { AdversaryName, AdversaryLevelId } from "@models/game/adversaries";
+import { ADVERSARIES } from "@data/adversaries";
+import { BOARDS } from "@data/boards";
+import { EXPANSIONS } from "@data/expansions";
+import { MAPS } from "@data/maps";
+import { SCENARIOS } from "@data/scenarios";
+import { SPIRITS } from "@data/spirits";
+
+describe("getValidCombos", () => {
+	it("returns possible combinations for lowest difficulty", () => {
+		const mockConfig: IConfig = {
+			expansions: EXPANSIONS,
+			players: 1,
+			difficultyRange: [0, 0],
+			spiritNames: SPIRITS.map(spirit => spirit.name),
+			mapNames: MAPS.map(map => map.name),
+			boardNames: BOARDS.map(board => board.name),
+			scenarioNames: SCENARIOS.map(scenario => scenario.name),
+			adversaryNamesAndIds: ADVERSARIES.reduce((model, adversary) => {
+				model.push(adversary.name);
+				adversary.levels.forEach(level => model.push(level.id));
+				return model;
+			}, [] as (AdversaryName | AdversaryLevelId)[]),
+		};
+		expect(getValidCombos(mockConfig)).toStrictEqual([
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{ "name": "No Scenario", "difficulty": 0 }
+			],
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{ "name": "Blitz", "difficulty": 0 }
+			],
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{ "name": "Guard the Isle's Heart", "difficulty": 0 }
+			],
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{"name": "Second Wave","difficulty": 0,"expansion": "Branch & Claw"
+				}
+			],
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{ "name": "A Diversity of Spirits", "difficulty": 0, "expansion": "Promo Pack 2" }
+			],
+			[
+				{ "name": "Balanced", "difficulty": 0 },
+				{ "id": "none", "name": "N/A", "difficulty": 0 },
+				{ "name": "Elemental Invocation", "difficulty": 0, "expansion": "Jagged Earth" }
+			]
+		]);
+	});
+
+	it("returns all possible combinations", () => {
+		const mockConfig: IConfig = {
+			expansions: EXPANSIONS,
+			players: 1,
+			difficultyRange: [0, 11],
+			spiritNames: SPIRITS.map(spirit => spirit.name),
+			mapNames: MAPS.map(map => map.name),
+			boardNames: BOARDS.map(board => board.name),
+			scenarioNames: SCENARIOS.map(scenario => scenario.name),
+			adversaryNamesAndIds: ADVERSARIES.reduce((model, adversary) => {
+				model.push(adversary.name);
+				adversary.levels.forEach(level => model.push(level.id));
+				return model;
+			}, [] as (AdversaryName | AdversaryLevelId)[]),
+		};
+		expect(getValidCombos(mockConfig)).toHaveLength(1215);
+	});
+});
