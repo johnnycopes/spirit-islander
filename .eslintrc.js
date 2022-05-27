f0bfd0e01e79de5654900cf607ddf43b8461e91d
John Coppola <jdcoppola@gmail.com>
2022-05-27 08:46:47 -0400

    eslint overrides

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking", "plugin:storybook/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:storybook/recommended"
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
@@ -13,10 +18,19 @@ module.exports = {
   },
   plugins: ["svelte3", "@typescript-eslint"],
   ignorePatterns: ["public/build/", "**/*.spec.ts"],
-  overrides: [{
-    files: ["**/*.svelte"],
-    processor: "svelte3/svelte3"
-  }],
+  overrides: [
+    {
+      files: ["**/*.svelte"],
+      processor: "svelte3/svelte3"
+    },
+    {
+      files: ["**/*.stories.js"],
+      rules: {
+        "@typescript-eslint/no-unsafe-assignment": 0,
+        "@typescript-eslint/no-unsafe-member-access": 0,
+      }
+    }
+  ],
   rules: {
     quotes: ["error", "double"],
     "@typescript-eslint/no-empty-interface": ["error", {
