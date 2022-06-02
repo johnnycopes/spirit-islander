c3ff7d58891c95027bc547e42ca02e10335b18ef
John Coppola <jdcoppola@gmail.com>
2022-06-02 07:13:25 -0400

    installs storybook and writes first story for button

---
+++ src/components/shared/Button.stories.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Button from "./Button.svelte";
+</script>
+
+<Meta
+  title="Shared/Button"
+  component={Button}
+  argTypes={{
+    slot: { control: "text" },
+    disabled: { control: "boolean" },
+    onClick: { action: "onClick" },
+  }}
+/>
+
+<Template let:args>
+  <Button
+    {...args}
+    on:click={args.onClick}
+  >
+    {args.slot}
+  </Button>
+</Template>
+
+<Story
+  name="Base"
+  args={{
+    slot: "Click me!",
+    disabled: false,
+  }}
+/>
